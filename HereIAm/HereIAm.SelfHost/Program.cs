3b763039b88e273a459b983068cf514165d6e451
James Stumme <jstumme@pillartechnology.com>
2015-07-23 16:31:05 -0400

    Added base project with self hosting project.

---
+++ HereIAm/HereIAm.SelfHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace HereIAm.SelfHost
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			using (var host = new NancyHost (new Uri("http://localhost:8080/"))) {
+				Console.Write ("Starting Nancy Host...");
+				host.Start ();
+				Console.WriteLine (" Done!");
+				Console.Write ("Press any key to stop.");
+				Console.ReadKey ();
+			}
+		}
+	}
+}
---
+++ HereIAm/HereIAm/NotificationModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy;
+
+namespace HereIAm
+{
+	public class NotificationModule : NancyModule
+	{
+		public NotificationModule ()
+		{
+			Get ["/notification"] = parameter => {
+				return "Notified!!!";
+			};
+		}
+	}
+}
+
