8c9f10c0153c40db873e680fadf77d09bae9bd0e
James Stumme <jstumme@pillartechnology.com>
2015-07-23 15:03:23 -0400

    Added initial Nancy web service project.

---
+++ HereIAm/HereIAm/MainModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nancy;
+
+namespace HereIAm
+{
+	public class MainModule : NancyModule
+	{
+		public MainModule ()
+		{
+			Get["/"] = parameters => { return "Hello World"; };
+		}
+	}
+}
+
---
+++ HereIAm/HereIAm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace HereIAm
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			using (var host = new NancyHost (new Uri ("http://localhost:8080/"))) {
+				host.Start ();
+				Console.ReadKey ();
+			}
+		}
+	}
+}
